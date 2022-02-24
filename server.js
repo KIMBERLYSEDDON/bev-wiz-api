1a846c6d22815373b56b70d370b9ee13ce25bd8b
Fikcup <rhys.n.wright@gmail.com>
2022-02-23 18:26:05 -0800

    feat : connected routes to server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 db.once('open', () => {
     console.log('Successfully connected to the database');
