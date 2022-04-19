5039823a4632bdf7f00d7c5653f04a0ac9a55abc
Alena Rehberger <alenarehberger92@gmail.com>
2022-04-18 22:12:46 -0500

    Added mixers and garnish data with routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 // TODO: add individual routes for different table requests
 
-// requiring alcohol, drinks data 
-const alcohol = require('./alcohol');
-const drinks = require('./drinks');
+// requiring alcohol, drinks, mixers, and garnish data
+const alcohol = require("./alcohol");
+const drinks = require("./drinks");
+const mixers = require("./mixers");
+const garnish = require("./garnish");
 
 // api/alcohol
 router.get("/alcohol", (req, res) => {
@@ -12,7 +14,17 @@ router.get("/alcohol", (req, res) => {
 
 // api/drinks
 router.get("/drinks", (req, res) => {
-    return res.send([drinks]);
-  });
+  return res.send([drinks]);
+});
+
+// api/mixers
+router.get("/mixers", (req, res) => {
+  return res.send([mixers]);
+});
+
+// api/garnish
+router.get("/garnish", (req, res) => {
+  return res.send([garnish]);
+});
 
 module.exports = router;
---
+++ routes/api/mixers.js
@@ -0,0 +1,86 @@
+// mixers data
+const mixers = [
+    {
+      id: 1,
+      name: "Tonic Water",
+      type: "Soda"
+    },
+    {
+      id: 2,
+      name: "Sweet and Sour Mix",
+      type: "Mix"
+    },
+    {
+      id: 3,
+      name: "Margarita Mix",
+      type: "Mix"
+    },
+    {
+      id: 4,
+      name: "Ginger Beer",
+      type: "Beer"
+    },
+    {
+      id: 5,
+      name: "Club Soda",
+      type: "Soda"
+    },
+    {
+      id: 6,
+      name: "Cola",
+      type: "Soda"
+    },
+    {
+      id: 7,
+      name: "Apple Cider",
+      type: "Cider"
+    },
+    {
+      id: 8,
+      name: "Grenadine",
+      type: "Juice"
+    },
+    {
+      id: 9,
+      name: "Orange Juice",
+      type: "Juice"
+    },
+    {
+      id: 10,
+      name: "Pineapple Juice",
+      type: "Juice"
+    },
+    {
+      id: 11,
+      name: "Tomato Juice",
+      type: "Juice"
+    },
+    {
+      id: 12,
+      name: "Fruit Pure",
+      type: "Pure"
+    },
+    {
+      id: 13,
+      name: "Lime Juice",
+      type: "Juice"
+    },
+    {
+      id: 14,
+      name: "Lemonade",
+      type: "Lemonade"
+    },
+    {
+      id: 15,
+      name: "Cranberry Juice",
+      type: "Juice"
+    },
+    {
+      id: 16,
+      name: "Grapefruit Juice",
+      type: "Juice"
+    }
+  ];
+
+
+  module.exports = mixers;
